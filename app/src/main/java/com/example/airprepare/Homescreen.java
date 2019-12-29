980c053492629b06d3bada867f7d1dbc6d53f659
Anudeep <basvaanu9125@gmail.com>
2019-12-29 12:10:54 +0530

    login success

--- app/src/main/java/com/example/airprepare/Homescreen.java
+++ app/src/main/java/com/example/airprepare/Homescreen.java
@@ -15,20 +15,7 @@ public class Homescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
-        Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show();
-        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("pref",0);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        String mobile = null;
-        editor.apply();
-        Intent i2=getIntent();
-        String mobno1=i2.getStringExtra("number1");
-        editor.putString("numb",mobno1);
-        if (sharedPreferences.getString("numb", null).length() == 0) {
 
-            Intent i1 = new Intent(this,MainActivity.class);
-            startActivity(i1);
-        }
+
     }
-
-
 }
--- app/src/main/java/com/example/airprepare/MainActivity.java
+++ app/src/main/java/com/example/airprepare/MainActivity.java
@@ -30,78 +30,75 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button createaccountb,login;
+    Button createaccountb, login;
     FirebaseAuth mAuth;
-    EditText number,password;
+    EditText number, password;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
     String num;
     String pas;
     ProgressDialog progressDialog;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createaccountb=(Button) findViewById(R.id.createaccountb);
-        number=(EditText) findViewById(R.id.number);
-        password=(EditText) findViewById(R.id.password);
-        login=(Button)findViewById(R.id.signinb);
-        firebaseAuth=FirebaseAuth.getInstance();
-        firebaseDatabase=FirebaseDatabase.getInstance();
-        progressDialog =new ProgressDialog(this);
+        createaccountb = (Button) findViewById(R.id.createaccountb);
+        number = (EditText) findViewById(R.id.number);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.signinb);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Loading...");
         progressDialog.setCanceledOnTouchOutside(false);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login.setClickable(false);
-                if(number.getText().toString()==null || number.getText().toString().equals("")&&password.getText().toString()==null||password.getText().toString().equals("")){
-                 Toast.makeText(MainActivity.this,"Inavlid Inputs",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    progressDialog.show();
-                   final String num,pass;
-                    num=number.getText().toString();
-                    pass=password.getText().toString();
-                    mAuth=FirebaseAuth.getInstance();
-                    String email=num+"@gmail.com";
-                    mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful())
-                            {
-                                Log.d("User Login","signInWithEmail::success");
-                                FirebaseUser user=mAuth.getCurrentUser();
-                                progressDialog.dismiss();
 
-                               Intent i=new Intent(MainActivity.this,Homescreen.class);
-                               i.putExtra("number1",num);
-                               startActivity(i);
-                            }
-                            else
-                            {
-                                Log.w("Student Login","signInWithEmail::failuer",task.getException());
-                                progressDialog.dismiss();
-                                Toast.makeText(MainActivity.this,"Authentication Failed",Toast.LENGTH_SHORT).show();
-
-                            }
-                        }
-                    });
-
-
-
-                }
-
-            }
-        });
+        if(firebaseAuth.getCurrentUser()!= null){
+            finish();
+            startActivity( new Intent(this,Homescreen.class));
+        }
 
     }
-
-
     public void Opennunberactivity(View view) {
         Intent i = new Intent(this,numberauthentication.class);
         startActivity(i);
     }
 
+    public void openHomescreen(View view) {
+        login.setClickable(false);
+        if (number.getText().toString() == null || number.getText().toString().equals("") && password.getText().toString() == null || password.getText().toString().equals("")) {
+            Toast.makeText(MainActivity.this, "Inavlid Inputs", Toast.LENGTH_SHORT).show();
+        } else {
+            progressDialog.show();
+            final String num, pass;
+            num = number.getText().toString();
+            pass = password.getText().toString();
+            mAuth = FirebaseAuth.getInstance();
+            String email = num + "@gmail.com";
+
+            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("User Login", "signInWithEmail::success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        progressDialog.dismiss();
+
+                        Intent i = new Intent(MainActivity.this, Homescreen.class);
+                        i.putExtra("number1", num);
+                        startActivity(i);
+                    } else {
+                        Log.w("Student Login", "signInWithEmail::failuer", task.getException());
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
     }
+
+}
+
+
